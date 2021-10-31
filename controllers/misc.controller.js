590e5347f469a52276ac03f714a996fce19d4a11
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-10-31 23:40:10 +0100

    Added Telegram bot messaging logic

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
 const { ContactTelegramBot } = require("../services/misc.services");
 
 module.exports = {
-  ContactUsController: (req, res) => {
+  ContactUsController: async (req, res) => {
+    const chat_id = process.env.TELEGRAM_CHAT_ID;
     // const { email, fullName, text, phone } = req;
-    const response = ContactTelegramBot();
+
+    const payload = {
+      chat_id,
+      text: "Hello, Ogbeni",
+    };
+
+    const response = await ContactTelegramBot(payload);
     res.status(response.status).json(response);
   },
 };
--- services/misc.services.js
+++ services/misc.services.js
@@ -1,10 +1,32 @@
+require("dotenv").config();
+
 module.exports = {
-  ContactTelegramBot: (payload) => {
-    // const { name, email, phone, text } = payload;
-    return {
-      status: "501",
-      code: "NOT_IMPLEMENTED",
-      message: "Telegram Feature not implemented",
+  ContactTelegramBot: async (payload) => {
+    const axios = require("axios");
+    let response = {
+      status: 500,
+      message:
+        "I'm Sorry, your message couldn't be delivered because of a technical issue. Please try again in a moment",
     };
+
+    const url = `https://api.telegram.org/${process.env.TELEGRAM_API_KEY}/sendMessage`;
+
+    await axios
+      .post(url, payload)
+      .then((res) => {
+        response = {
+          status: 200,
+          message: "Message delivered. Thank you for getting in touch with me!",
+        };
+      })
+      .catch((error) => {
+        response = {
+          status: 400,
+          message:
+            "I'm Sorry, your message couldn't be delivered because of a technical issue. Please try again in a moment",
+        };
+      });
+
+    return response;
   },
 };
