afc602d0b3b219b16ce175c173da8e4b73a5ebc6
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-11-01 12:29:46 +0100

    Allow CORS

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 /* Route handlers */
 app.use("/", utilRoutes);
 app.use("/misc", miscRoutes);
